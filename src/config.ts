99ff1a2c14933c60d9e7d26193993ec00edab14e
theprogrammergary <theprogrammergary@gmail.com>
2024-01-14 09:53:57 -0500

    add guild id to .env

--- src/config.ts
+++ src/config.ts
@@ -2,11 +2,12 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const { BOT_TOKEN } = process.env;
-if (!BOT_TOKEN) {
+const { BOT_TOKEN, GUILD_ID } = process.env;
+if (!BOT_TOKEN || !GUILD_ID) {
   throw new Error("Missing environment variables");
 }
 
 export const config = {
   BOT_TOKEN,
+  GUILD_ID,
 };
