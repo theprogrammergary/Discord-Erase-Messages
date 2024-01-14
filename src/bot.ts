3e209316584aa4e3ea1f2866da7c0735a8541d75
theprogrammergary <theprogrammergary@gmail.com>
2024-01-14 09:53:49 -0500

    get guild from ID

--- src/bot.ts
+++ src/bot.ts
@@ -1,18 +1,27 @@
-import { Client, GatewayIntentBits } from "discord.js";
+import { Client, GatewayIntentBits, Guild } from "discord.js";
 import { config } from "./config";
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
 });
 
-client.once("ready", () => {
+client.once("ready", async () => {
   console.log("Discord bot is ready! 🤖");
+  const guild: Guild = await getGuild(config.GUILD_ID);
+  await getGuildMessages(guild);
 });
 
-client.on("messageCreate", (message) => {
-  if (message.content === "!ping") {
-    message.reply("Pong!");
+async function getGuildMessages(guild: Guild) {
+  console.log(`Ready to get "${guild.name}" messages`);
+}
+
+async function getGuild(guildId: string): Promise<Guild> {
+  const guild: Guild | undefined = client.guilds.cache.get(guildId);
+  if (guild) {
+    return guild;
+  } else {
+    throw new Error(`Guild with ID ${guildId} not found!`);
   }
-});
+}
 
 client.login(config.BOT_TOKEN);
