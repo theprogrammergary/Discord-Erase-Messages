1af9823d0e36396fba6b0c31f8906d11f84641c1
theprogrammergary <theprogrammergary@gmail.com>
2024-01-15 09:51:35 -0500

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,12 +2,22 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const { BOT_TOKEN, GUILD_ID } = process.env;
+const { BOT_TOKEN, GUILD_ID, MEMBER_ID_1, MEMBER_ID_2 } = process.env;
 if (!BOT_TOKEN || !GUILD_ID) {
   throw new Error("Missing environment variables");
 }
 
+interface MemberIDs {
+  id1: string;
+  id2: string;
+}
+const memberIds: MemberIDs = {
+  id1: process.env.MEMBER_ID_1 || "",
+  id2: process.env.MEMBER_ID_2 || "",
+};
+
 export const config = {
   BOT_TOKEN,
   GUILD_ID,
+  memberIds,
 };
